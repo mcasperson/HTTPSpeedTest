b65470d77bd21f30a71410eefd31de6bcc0833dc
Matthew Casperson <matthewcasperson@gmail.com>
2013-09-13 11:15:34 +1000

    Initial import

---
+++ src/EmptyServlet.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class EmptyServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+}
